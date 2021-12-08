49cfca722bbed728ceb6e11f099740ebef8550f8
Andrey Onokhov <andrey.onokhov@gmail.com>
2021-12-08 16:01:34 +0300

    rename images name and modify code

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 var cvs = document.getElementById('canvas');
 
-var ctx = cvs.getContex('2d');
+var ctx = cvs.getContext('2d');
 
 var bird = new Image();
 var bg = new Image();
@@ -11,5 +11,20 @@ var pipeBottom = new Image();
 bird.src = 'img/bird.png';
 bg.src = 'img/bg.png';
 fg.src = 'img/fg.png';
-pipeUp.src = 'img/pipeNorth.png';
-pipeBottom.src = 'img/pipeSouth.png';
+pipeUp.src = 'img/pipeUp.png';
+pipeBottom.src = 'img/pipeBottom.png';
+
+var gap = 90;
+
+function draw() {
+  ctx.drawImage(bg, 0, 0);
+
+  ctx.drawImage(pipeUp, 100, 0);
+  ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+  ctx.drawImage(fg, 0, cvs.height - fg.height);
+  
+  ctx.drawImage(bird, 10, 150);
+}
+
+pipeBottom.onload = draw;

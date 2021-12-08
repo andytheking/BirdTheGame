c193e8ec91d23ac61068c50820b77b6927d4f6e6
Andrey Onokhov <andrey.onokhov@gmail.com>
2021-12-08 16:30:52 +0300

    odify conditions of a game

--- js/game.js
+++ js/game.js
@@ -16,15 +16,58 @@ pipeBottom.src = 'img/pipeBottom.png';
 
 var gap = 90;
 
+// if user push any button
+document.addEventListener('keydown', moveUp);
+
+function moveUp() {
+  yPos -= 20;
+}
+
+// blocks creating
+var pipe = [];
+pipe[0] = {
+  x : cvs.width,
+  y : 0
+}
+
+// position of a bird
+var xPos = 10;
+var yPos = 150;
+var grav = 1.5;
+
 function draw() {
   ctx.drawImage(bg, 0, 0);
 
-  ctx.drawImage(pipeUp, 100, 0);
-  ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+  for(var i = 0; i < pipe.length; i++) {
+    ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+    ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+    pipe[i].x--;
+
+    if(pipe[i].x == 125) {
+      pipe.push({
+        x: cvs.width,
+        y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+      });
+    }
+
+// if a bird touch a pipe
+    if(xPos + bird.width >= pipe[i].x
+      && xPos <= pipe[i].x + pipeUp.width
+      && (yPos <=pipe[i].y + pipeUp.height
+        || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)) {
+          location.reload(); /* page reload */
+        }
+
+
+  }
 
   ctx.drawImage(fg, 0, cvs.height - fg.height);
-  
-  ctx.drawImage(bird, 10, 150);
+
+  ctx.drawImage(bird, xPos, yPos);
+
+  yPos += grav;
+  requestAnimationFrame(draw);
 }
 
 pipeBottom.onload = draw;

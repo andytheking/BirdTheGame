124aa56b1c9cc66bc08ca60547ae7dbc7852a4e1
Andrey Onokhov <andrey.onokhov@gmail.com>
2021-12-08 15:47:23 +0300

    modified add images to the code

--- js/game.js
+++ js/game.js
@@ -1,1 +1,15 @@
-alert('one');
+var cvs = document.getElementById('canvas');
+
+var ctx = cvs.getContex('2d');
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = 'img/bird.png';
+bg.src = 'img/bg.png';
+fg.src = 'img/fg.png';
+pipeUp.src = 'img/pipeNorth.png';
+pipeBottom.src = 'img/pipeSouth.png';
